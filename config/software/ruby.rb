50b529917567ac061f8868f40b9d6122b89a2ba5
Scott Sanders <scott@jssjr.com>
2012-07-15 23:13:18 -0400

    just a test

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -49,6 +49,7 @@ env =
 
 build do
   # command "#{install_dir}/embedded/bin/autoconf", :env => env
+  command "echo $ENV"
   command "./configure --prefix=#{install_dir}/embedded --with-opt-dir=#{install_dir}/embedded --enable-shared --disable-install-doc", :env => env
   command "make -j #{max_build_jobs}", :env => env
   command "make install", :env => env
